b638a9d8b7b7ce3d42e9d7aa13b6fa3c2d18f746
andytyler <atyler1@sheffield.ac.uk>
2019-08-10 22:02:46 +0100

    Messing abouts learning to be transoformed into the learning repo

--- repo-select.js
+++ repo-select.js
@@ -3,14 +3,19 @@ const exec = require('child_process').exec
 const path = require('path')
 const fs = require('fs')
 const process = require('process')
+const chalk = require('chalk')
 
 const allParamsArray = process.argv
 const paramOneIndex = process.argv.indexOf('-w')
 
 const filepathofpack = allParamsArray[paramOneIndex + 1]
 
-console.log(`${filepathofpack}, is the package.json that will be changed!`)
-console.log(allParamsArray)
+console.log(
+	chalk.magenta(
+		`${filepathofpack}, is the package.json that will be changed!`
+	)
+)
+console.log(chalk.bold(allParamsArray))
 
 // exec('cat repo-select.js', (error, stdout, stderr) => {
 // 	stdout ? console.log(stdout) : console.log('nope')
@@ -40,11 +45,11 @@ var file = fs.readFileSync(filepathofpack, 'utf8')
 // JSON.parse(file).red = 'New Red'
 const newFile = JSON.parse(file)
 newFile.title = 'blast off'
-console.log(newFile)
+console.log(chalk.gray(JSON.stringify(newFile, null, 4)))
 
 // console.log(JSON.parse(file).red)
 fs.writeFile(filepathofpack, JSON.stringify(newFile, null, 4), err => {
-	err ? console.error(err) : console.log('saved')
+	err ? console.error(err) : console.log(chalk.bold.bgGreen('saved'))
 })
 // console.log(file)
 // console.log(process.cwd())
@@ -52,5 +57,5 @@ fs.writeFile(filepathofpack, JSON.stringify(newFile, null, 4), err => {
 // console.log(file.red)
 
 process.on('exit', code => {
-	console.log(`About to exit with code ${code}`)
+	console.log(chalk.bold.red(`About to exit with code ${code}`))
 })
