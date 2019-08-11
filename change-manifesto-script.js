8c0c05124239e5f3836170c4560ba26578893e42
andytyler <atyler1@sheffield.ac.uk>
2019-08-11 08:38:56 +0100

    Made file execuatable

---
+++ change-manifesto-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const exec = require('child_process').exec
+const path = require('path')
+const fs = require('fs')
+const process = require('process')
+const chalk = require('chalk')
+
+console.log('hello')
